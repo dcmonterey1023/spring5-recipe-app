5a645ca45199d037a94f2068c7c54d9dab9ae254
Dyun Wilson Monterrey/Knox Server Development /SRPH/Engineer/삼성전자 <d.monterey@samsung.com>
2020-07-10 21:06:19 +0800

    created enum Difficulty and added to Recipe using @Enumeration. overriden the enum default value to String - EnumType.STRING

---
+++ src/main/java/guru/springframework/model/Difficulty.java
@@ -0,0 +1,8 @@
+package guru.springframework.model;
+
+/**
+ * @author d.monterey on 10/07/2020
+ */
+public enum Difficulty {
+    EASY, MODERATE, KIND_OF_HARD, HARD
+}
--- src/main/java/guru/springframework/model/Recipe.java
+++ src/main/java/guru/springframework/model/Recipe.java
@@ -19,15 +19,18 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //todo add
-    //private Difficulty difficulty;
+
     @Lob
     private Byte[] image;
+
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     public Long getId() {
         return id;
@@ -107,5 +110,13 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
